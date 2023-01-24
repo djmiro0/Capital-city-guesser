1cdfa8287cb8dc5117771096438958598dbdcabe
tareq-almasri <talmasreq@gmail.com>
2023-01-24 10:56:49 +0100

    solution updated

--- 24.01 forms/Browser-DynamicPills-solution/js/index.js
+++ 24.01 forms/Browser-DynamicPills-solution/js/index.js
@@ -1,7 +1,7 @@
 let pills = document.querySelectorAll('li');
 let movies = document.querySelectorAll('section');
 
-let checkAndRemoveClass = function(array, ...classToCheck) {
+let checkAndRemoveClass = function(array, classToCheck) {
   for (let k = 0; k < array.length; k++) {
     if (array[k].classList.contains(classToCheck)) {
       array[k].classList.remove(classToCheck);
@@ -14,6 +14,7 @@ for (let i = 0; i < pills.length; i++) {
   pills[i].addEventListener('click', function(ev) {
     checkAndRemoveClass(movies, 'active');
     checkAndRemoveClass(pills, 'selected');
+
     pills[i].classList.add('selected');
     movies[i].classList.add('active');
   });
