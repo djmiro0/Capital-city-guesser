879283229d5fbc9e1e8a14c0c1753f6778644aa1
tareq-almasri <talmasreq@gmail.com>
2023-01-31 13:58:21 +0100

    updates

--- 31.01 GET-req/fetch/index.js
+++ 31.01 GET-req/fetch/index.js
@@ -48,7 +48,7 @@ async function fetchCountryByName(e) {
       population.innerText = data[0].population;
       area.innerText = data[0].area;
       region.innerText = data[0].region;
-      capital.innerText = data[0].capital;
+      capital.innerText = data[0]["capital"];
 
   } catch (err) {
     console.log(err);
@@ -63,3 +63,43 @@ fetch("https://api.coinconvert.net/convert/eth/eur?amount=10")
   .then((response) => response.json())
   .then((data) => console.log(data))
   .catch((err) => console.log(err));
+
+//==================================================
+//getting all the countries on click and display them in the browser:
+//API URL: https://restcountries.com/v2/all
+
+const btnAll = document.querySelector("#all-btn");
+
+const getAllCountries = async () => {
+  try {
+    let response = await fetch("https://restcountries.com/v2/all");
+    let data = await response.json();
+
+    data.forEach((item) => {
+      const box = document.createElement("div");
+
+      box.innerHTML = `<h1> ${item.name} </h1>
+
+<img src=${item.flags.png} />
+<h3>Capital:</h3>
+<div id="capital">
+${item.capital}
+</div>
+<h3>Population: </h3>
+<div id="population">
+${item.population}
+</div>
+<h3>Area: </h3>
+<div id="area"></div>
+<h3>Region: </h3>
+<div id="region"></div>
+`;
+      document.body.appendChild(box);
+    });
+    //    console.log(data)
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+btnAll.addEventListener("click" ,getAllCountries)
