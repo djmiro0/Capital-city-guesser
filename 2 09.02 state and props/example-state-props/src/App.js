0223fc6e94aafb91f3b403f5243105877c145c4a
tareq-almasri <talmasreq@gmail.com>
2023-02-13 12:25:12 +0100

    live coding 13.02

--- 2 09.02 state and props/example-state-props/src/App.js
+++ 2 09.02 state and props/example-state-props/src/App.js
@@ -1,20 +1,23 @@
-import 'bootstrap/dist/css/bootstrap.min.css';
-import './App.css';
-import Navbar from './components/Navigation/Navbar';
-import { useState } from 'react';
-import PostList from './components/PostList/PostList';
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+import Navbar from "./components/Navigation/Navbar";
+import { useState } from "react";
+import PostList from "./components/PostList/PostList";
 
 function App() {
-  const [posts, setPosts] = useState([{title:'Hello', description:'This is a test description'}, {title:'Hello', description:'This is a test description'}]);
+  const [posts, setPosts] = useState([
+    { title: "Hello", description: "This is a test description" },
+    { title: "Hello", description: "This is a test description" },
+  ]);
 
   return (
     <div className="App">
-        <Navbar title="Homepage - Welcome" language="DE" color="red" />
+      <Navbar title="Homepage - Welcome" language="DE" color="red" />
 
-        <div className='container'>
-          <CreatePost postsprop={posts} setPosts={setPosts} />
-          <PostList postsprop={posts} />
-        </div>
+      <div className="container">
+        <CreatePost postsprop={posts} setPosts={setPosts} />
+        <PostList postsprop={posts} />
+      </div>
     </div>
   );
 }
--- 2 09.02 state and props/example-state-props/src/components/CreatePost/CreatePost.jsx
+++ 2 09.02 state and props/example-state-props/src/components/CreatePost/CreatePost.jsx
@@ -1,33 +1,45 @@
-import React, { useState } from 'react'
+import React, { useState, Fragment }  from "react";
 
-export default function CreatePost({postsprop, setPosts}) {
+export default function CreatePost({ postsprop, setPosts }) {
   const [title, setTitle] = useState();
   const [description, setDescription] = useState();
 
   const addPost = () => {
-    setPosts([...postsprop, {title, description}])
-  }
+    setPosts([...postsprop, { title, description }]);
+  };
 
   return (
-<div>
-    <div className='row'>
-        <div className='col'>
-            <label for="exampleFormControlInput1" className="form-label">Title</label>
-            <input 
-                type="text" 
-                className='form-control' 
-                onChange={(e) => setTitle(e.target.value)} />
-            <label for="exampleFormControlInput1" className="form-label">Description</label>
-            <input 
-                type="text" 
-                className='form-control' 
-                onChange={(e) => setDescription(e.target.value)} />
-            </div>
+    <Fragment>
+    <div>
+      <div className="row">
+        <div className="col">
+          <label for="exampleFormControlInput1" className="form-label">
+            Title
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={(e) => setTitle(e.target.value)}
+          />
+          <label for="exampleFormControlInput1" className="form-label">
+            Description
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            onChange={(e) => setDescription(e.target.value)}
+          />
+        </div>
 
-        <button onClick={addPost} className='btn btn-primary'>
-            Create post
+        <button onClick={addPost} className="btn btn-primary">
+          Create post
         </button>
+      </div>
     </div>
-   </div>
-  )
+    </Fragment>
+  );
 }
+
+
+
+
