ed1e654472adae9da22d28280b5149396dfa66dc
tareq-almasri <talmasreq@gmail.com>
2023-02-20 10:08:55 +0100

    second commit

--- 2 20.02 react-router/routing/src/App.js
+++ 2 20.02 react-router/routing/src/App.js
@@ -1,9 +1,24 @@
-import './App.css';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Home from "./components/views/Home";
+import Login from "./components/views/Login";
+import Signup from "./components/views/Signup";
 
 function App() {
   return (
     <div className="App">
-      hello world
+      <Header />
+
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route path="*" element={<div> 404 page not found </div>} />
+      </Routes>
+
+      <Footer />
     </div>
   );
 }
