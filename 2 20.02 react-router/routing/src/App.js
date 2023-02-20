5504084010ca5a72767b9c0df25120736e72d920
tareq-almasri <talmasreq@gmail.com>
2023-02-20 10:37:23 +0100

    signup form part1

--- 2 20.02 react-router/routing/src/App.js
+++ 2 20.02 react-router/routing/src/App.js
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
+import About from "./components/views/About";
 import Home from "./components/views/Home";
 import Login from "./components/views/Login";
 import Signup from "./components/views/Signup";
@@ -15,6 +16,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<Signup />} />
+        <Route path="/about" element={<About/>} />
         <Route path="*" element={<div> 404 page not found </div>} />
       </Routes>
 
--- 2 20.02 react-router/routing/src/components/views/Signup.js
+++ 2 20.02 react-router/routing/src/components/views/Signup.js
@@ -1,7 +1,19 @@
-import React from 'react'
+import { useState } from "react";
 
 export default function Signup() {
+  const [user, setUser] = useState({ username: "", email: "", password: "" });
+
   return (
-    <div>Signup</div>
-  )
+    <form>
+      <h1>Sign up</h1>
+      <h3>Username:</h3>
+      <input
+        type="text"
+        onChange={(e) =>   
+          setUser((prev) => ({ ...prev, username: e.target.value }))
+        }  
+      />
+      {/* setUser({...user, username: e.target.value})*/}
+    </form>
+  );
 }
