fe9d067e75a89a090d23480bb5d817354fde7b0f
tareq-almasri <talmasreq@gmail.com>
2023-02-20 11:18:40 +0100

    link

--- 2 20.02 react-router/routing/src/components/Header.js
+++ 2 20.02 react-router/routing/src/components/Header.js
@@ -1,7 +1,13 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Header() {
   return (
-    <div>Header</div>
-  )
+    <div>
+      <Link to="/"> Home </Link>
+      <Link to="/about"> About </Link>
+      <Link to="/signup"> Sing up </Link>
+      <Link to="/login"> Login </Link>
+    </div>
+  );
 }
--- 2 20.02 react-router/routing/src/components/views/Signup.js
+++ 2 20.02 react-router/routing/src/components/views/Signup.js
@@ -3,17 +3,50 @@ import { useState } from "react";
 export default function Signup() {
   const [user, setUser] = useState({ username: "", email: "", password: "" });
 
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    fetch('https://jsonplaceholder.typicode.com/users', {
+      method: 'POST',
+      body: JSON.stringify(user),
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+      },
+    })
+      .then((response) => response.json())
+      .then((data) => console.log(data));
+  }
+
   return (
-    <form>
+    <form
+      onSubmit={handleSubmit}
+    >
       <h1>Sign up</h1>
       <h3>Username:</h3>
       <input
+        required
         type="text"
-        onChange={(e) =>   
-          setUser((prev) => ({ ...prev, username: e.target.value }))
-        }  
+        onChange={
+          (e) => setUser((prev) => ({ ...prev, username: e.target.value }))
+          //similar to: setUser({...user, username: e.target.value})
+        }
       />
-      {/* setUser({...user, username: e.target.value})*/}
+      <h3>Email:</h3>
+      <input
+        required
+        type="email"
+        onChange={(e) =>
+          setUser((prev) => ({ ...prev, email: e.target.value }))
+        }
+      />
+      <h3>Password:</h3>
+      <input
+        required
+        type="password"
+        onChange={(e) =>
+          setUser((prev) => ({ ...prev, password: e.target.value }))
+        }
+      />
+      <button> Signup </button>
     </form>
   );
 }
