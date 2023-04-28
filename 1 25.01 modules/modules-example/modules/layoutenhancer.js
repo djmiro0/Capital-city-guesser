9cd110b8063d5bbe90f31b0a275a783ec76bfd7f
Djordje <107116545+djmiro0@users.noreply.github.com>
2023-04-28 08:49:30 +0200

    Delete 1 25.01 modules/modules-example directory

--- 1 25.01 modules/modules-example/modules/layoutenhancer.js
+++
@@ -1,9 +0,0 @@
-/**
- * Sets the backgroundcolor of an element by ID. 
- * @param {*} elementId 
- * @param {*} backgroundColor 
- */
-export const setBackgroundColor = (elementId, backgroundColor) => {
-    const element = document.getElementById(elementId);
-    element.style.backgroundColor = backgroundColor;
-}
