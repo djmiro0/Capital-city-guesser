e018c7f81551268ae16ca49fa710cf4d7234aaff
Djordje <107116545+djmiro0@users.noreply.github.com>
2023-04-28 08:52:09 +0200

    Delete 2 23.02 context-example-use-case directory

--- 2 23.02 context-example-use-case/solution-spa-language/myapp/src/components/Content/Content.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import WelcomeMessage from '../WelcomeMessage/WelcomeMessage'
-
-export default function Content() {
-  return (
-    <div><WelcomeMessage /></div>
-  )
-}
--- 2 23.02 context-example-use-case/solution-spa-language/myapp/src/components/Header/Header.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import LanguageChooser from '../LanguageChooser/LanguageChooser'
-
-export default function Header() {
-  return (
-    <div><LanguageChooser /></div>
-  )
-}
