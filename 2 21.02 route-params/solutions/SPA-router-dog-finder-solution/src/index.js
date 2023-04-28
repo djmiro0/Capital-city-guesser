12c73b33b29b062c2410e1e38b046ceab9ebf29b
Djordje <107116545+djmiro0@users.noreply.github.com>
2023-04-28 08:51:50 +0200

    Delete 2 21.02 route-params directory

--- 2 21.02 route-params/solutions/SPA-router-dog-finder-solution/src/index.js
+++
@@ -1,7 +0,0 @@
-import reactDOM from "react-dom"
-import { BrowserRouter } from 'react-router-dom'
-import App from "./components/App.jsx"
-
-import './main.scss'
-
-reactDOM.render(<BrowserRouter><App/></BrowserRouter>,document.getElementById("root"))
